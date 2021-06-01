0afc329288d4fe1e9a07a3a825ed9b4456068a4f
Arnas Dickus <arnasdickus@protonmail.com>
2021-06-01 19:56:03 +0300

    added basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from './pages/home/home';
+import TailWind from './pages/tailwind/tailwind';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Switch>
+          <Route path="/" exact={true}>
+            <Home />
+          </Route>
+          <Route path="/tail-wind">
+            <TailWind />
+          </Route>
+        </Switch>
     </div>
   );
 }
---
+++ src/pages/tailwind/tailwind.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TailWindPage = () => {
+    return (
+        <div>
+            <p>This is Tailwind sandbox</p>
+        </div>
+    )
+}
+
+export default TailWindPage
